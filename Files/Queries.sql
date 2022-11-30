f7cbc4b02ce81c6ca5fc3d5a568196aa78c0af9d
999SH <viktor.salomonsson12@gmail.com>
2022-11-30 05:48:03 -0500

    First query done

--- Files/Queries.sql
+++ Files/Queries.sql
@@ -1,1 +1,16 @@
-SELECT id FROM TAG_TABLE WHERE 'aaaaaaaa' ~ tag_name
+
+/*Select total lessons*/
+
+SELECT DISTINCT month,
+SUM(COUNT)
+OVER (PARTITION BY month) AS monthlyLessons
+FROM(
+    SELECT COUNT(id), month 
+    FROM lesson
+    GROUP BY month
+    UNION
+    SELECT COUNT(id), month 
+    FROM ensemble
+    GROUP BY month
+) AS f;
+

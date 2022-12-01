3d5c1e590ed3f0eef11692cf161acab7324b942f
999SH <viktor.salomonsson12@gmail.com>
2022-12-01 15:21:04 -0500

    Task 3 finito

--- Files/Queries.sql
+++ Files/Queries.sql
@@ -14,90 +14,47 @@ FROM(
     GROUP BY month
 ) AS f;
 
-/* month | monthlylessons 
+/* 
+ month | monthlylessons 
 -------+----------------
  01    |              9
  02    |              4
  03    |              1
  07    |              1
- 08    |              1 */
+ 08    |              1 
+ */
 
 
 
-/*list group and individual lessons*/
 
-SELECT month, COUNT(gid) AS group_lessons, COUNT(iid) AS individual_lessons
+/* Lists group, individual and ensembles */ 
+
+SELECT month, COUNT(DISTINCT gid) AS group_lessons, 
+COUNT(DISTINCT iid) AS individual_lessons, COUNT (DISTINCT eid) AS ensembles
 FROM 
 (
-SELECT group_lesson.lesson_id AS gid, individual_lesson.lesson_id AS iid
-FROM individual_lesson
+SELECT group_lesson.lesson_id AS gid, individual_lesson.lesson_id AS iid, ensemble.id AS eid, lesson.month
+FROM lesson
+FULL OUTER JOIN individual_lesson
+ON lesson.id = individual_lesson.lesson_id
 FULL OUTER JOIN group_lesson
-ON individual_lesson.lesson_id = group_lesson.lesson_id
-) as f
-JOIN lesson
-ON lesson.id = iid 
-OR lesson.id = gid
-GROUP BY month
-ORDER BY month;
-
-/* month | group_lessons | individual_lessons 
--------+---------------+--------------------
- 01    |             3 |                  3
- 02    |             3 |                  0
- 07    |             1 |                  0
- 08    |             1 |                  0 */
-
-
-
-/* Lists group, individual and ensembles, broken due to ensembles using own months */ 
-
-SELECT month, COUNT(gid) AS group_lessons, COUNT(iid) AS individual_lessons, COUNT (eid) AS ensembles
-FROM 
-(
-SELECT group_lesson.lesson_id AS gid, individual_lesson.lesson_id AS iid, ensemble.id AS eid
-FROM individual_lesson
-JOIN lesson
-ON lesson.id = iid 
-FULL OUTER JOIN group_lesson
-ON individual_lesson.lesson_id = group_lesson.lesson_id
-JOIN lesson
-ON lesson.id = gid 
+ON lesson.id = group_lesson.lesson_id
 FULL OUTER JOIN ensemble
-ON ensemble.month = individual_lesson.month
+ON ensemble.month = lesson.month
 ) as f
-
-OR lesson.id = gid
 GROUP BY month
 ORDER BY month;
 
-/* month | group_lessons | individual_lessons | ensembles 
+ /* 
+ month | group_lessons | individual_lessons | ensembles 
 -------+---------------+--------------------+-----------
- 01    |             3 |                  3 |         0
- 02    |             3 |                  0 |         0
+ 01    |             3 |                  6 |         3
+ 02    |             3 |                  0 |         1
  07    |             1 |                  0 |         0
- 08    |             1 |                  0 |         0 */
-
-
-
-/* Only ensemble since they dont show up in the other one */
-
-SELECT DISTINCT month,
-SUM(COUNT)
-OVER (PARTITION BY month) AS monthlyLessons
-FROM(
-    SELECT COUNT(id), month 
-    FROM ensemble
-    GROUP BY month
-    ORDER BY month
-) AS f;
-
-/* month | monthlylessons 
--------+----------------
- 02    |              1
- 03    |              1
- 01    |              3 */
+ 08    |             1 |                  0 |         0
+       |             0 |                  0 |         1 
+*/
  
-
 
  /* See how many siblings each student has*/
 
@@ -107,13 +64,15 @@ WHERE student.person_id = sibling.person_id
 GROUP BY student
 ORDER BY student;
 
-/* student | siblings 
+/* 
+ student | siblings 
 ---------+----------
    21111 |        1
    21112 |        1
    21119 |        2
    21120 |        2
-   21121 |        2 */
+   21121 |        2 
+*/
 
 
 
@@ -125,7 +84,8 @@ WHERE student.person_id = sibling.person_id
 ORDER BY student;
 
 
-/* student | sibling 
+/*
+ student | sibling 
 ---------+---------
    21111 |   11111
    21112 |   11112
@@ -134,12 +94,16 @@ ORDER BY student;
    21120 |   11120
    21120 |   11120
    21121 |   11121
-   21121 |   11121 */
+   21121 |   11121 
+*/
 
 
 
 /* Show amount of lessons per instructor, inner query gives all booked lessons once */
 
+
+SELECT * 
+FROM(
 SELECT DISTINCT id AS instructor, 
 COUNT(lesson_id)
 OVER (PARTITION BY id) AS amount_of_lessons, month
@@ -167,15 +131,65 @@ INNER JOIN ensemble
 ON ensemble.id = scheduled_ensemble.ensemble_id
 ) 
 AS f
-WHERE month = '01'
-ORDER BY instructor;
+WHERE CAST(month AS INT) = (date_part('month',CURRENT_DATE))
+ORDER BY instructor
+) as g
+WHERE (amount_of_lessons > 1);
 
-/* instructor | amount_of_lessons | month 
+
+/*Month can be changed to an int for testing */
+
+/* 
+
+ instructor | amount_of_lessons | month 
 ------------+-------------------+-------
       91901 |                 1 | 01
       91902 |                 2 | 01
       91903 |                 1 | 01
       91904 |                 3 | 01
       91905 |                 2 | 01
-      91906 |                 2 | 01     */
+      91906 |                 2 | 01     
+      
+
+
+Current month with limit set to 1 below
+
+instructor | amount_of_lessons | month 
+------------+-------------------+-------
+      91902 |                 2 | 12     
+*/
+
+
+
+
+SELECT DISTINCT id, 
+(max-(COUNT(students_attending) OVER (PARTITION BY id))) AS places_left,
+max, genre, schedule,
+COUNT(students_attending) OVER (PARTITION BY id) AS students_attending,
+day, month  
+FROM 
+(
+SELECT ensemble.id, student_id AS students_attending, CAST(ensemble.max_students AS INT) AS max, 
+ensemble.day, ensemble.month, genre, schedule
+FROM ensemble 
+FULL OUTER JOIN  scheduled_ensemble
+ON ensemble.id = scheduled_ensemble.ensemble_id
+JOIN genre
+ON ensemble.genre_id = genre.id
+WHERE CAST(day AS INT) BETWEEN date_part('day',CURRENT_DATE)
+AND  date_part('day',CURRENT_DATE+6)
+AND CAST(month AS INT) = date_part('month',CURRENT_DATE)
+AND CAST(YEAR AS INT)  = date_part('year',CURRENT_DATE)
+ORDER BY genre_id, day
+) as f;
+
+/*
+  id   | places_left | max | genre | schedule | students_attending | day | month 
+-------+-------------+-----+-------+----------+--------------------+-----+-------
+ 22006 |           2 |   4 | RnB   | Friday   |                  2 | 02  | 12
+ 22007 |           3 |   4 | RnB   | Monday   |                  1 | 05  | 12
+*/
+
+
+
 

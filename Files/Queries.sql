89d2398ba3db58d335ef95148a2ccb23753a2465
999SH <94057132+999SH@users.noreply.github.com>
2022-12-02 15:27:01 +0100

    Final update report almost done

--- Files/Queries.sql
+++ Files/Queries.sql
@@ -1,77 +1,75 @@
 
 /*Select total lessons*/
 
-SELECT DISTINCT month,
-SUM(COUNT)
-OVER (PARTITION BY month) AS total_monthly_Lessons
-FROM(
-    SELECT COUNT(id), month 
-    FROM lesson
-    GROUP BY month
-    UNION
-    SELECT COUNT(id), month 
-    FROM ensemble
-    GROUP BY month
-) AS f;
 
-/* 
- month | monthlylessons 
--------+----------------
- 01    |              9
- 02    |              4
- 03    |              1
- 07    |              1
- 08    |              1 
- */
-
-
-
-
-/* Lists group, individual and ensembles */ 
+/* Lists group, individual, ensembles and total */ 
 
 SELECT month, COUNT(DISTINCT gid) AS group_lessons, 
-COUNT(DISTINCT iid) AS individual_lessons, COUNT (DISTINCT eid) AS ensembles
+COUNT(DISTINCT iid) AS individual_lessons, COUNT (DISTINCT eid) AS ensembles, 
+(COUNT(DISTINCT gid)+COUNT(DISTINCT iid)+COUNT (DISTINCT eid)) AS total_lessons
 FROM 
 (
-SELECT group_lesson.lesson_id AS gid, individual_lesson.lesson_id AS iid, ensemble.id AS eid, lesson.month
+SELECT group_lesson.lesson_id AS gid, individual_lesson.lesson_id AS iid, ensemble.id AS eid, lesson.month, lesson.year
 FROM lesson
 FULL OUTER JOIN individual_lesson
 ON lesson.id = individual_lesson.lesson_id
 FULL OUTER JOIN group_lesson
 ON lesson.id = group_lesson.lesson_id
 FULL OUTER JOIN ensemble
-ON ensemble.month = lesson.month
-) as f
+ON  lesson.month = ensemble.month
+) AS f
+WHERE CAST(year AS INT) = '2023'
 GROUP BY month
 ORDER BY month;
 
  /* 
- month | group_lessons | individual_lessons | ensembles 
--------+---------------+--------------------+-----------
- 01    |             3 |                  6 |         3
- 02    |             3 |                  0 |         1
- 07    |             1 |                  0 |         0
- 08    |             1 |                  0 |         0
-       |             0 |                  0 |         1 
+
+ month | group_lessons | individual_lessons | ensembles | total_lessons
+-------+---------------+--------------------+-----------+---------------
+ 01    |             3 |                  6 |         3 |            12
+ 02    |             3 |                  1 |         1 |             5
+ 03    |             0 |                  1 |         1 |             2
+ 04    |             0 |                  1 |         0 |             1
+ 05    |             0 |                  1 |         0 |             1
+ 06    |             0 |                  1 |         0 |             1
+ 07    |             1 |                  2 |         0 |             3
+ 08    |             1 |                  1 |         0 |             2
+ 09    |             0 |                  1 |         0 |             1
+ 10    |             0 |                  1 |         0 |             1
+ 11    |             0 |                  1 |         0 |             1
+ 12    |             0 |                  1 |         4 |             5
+
 */
  
 
  /* See how many siblings each student has*/
 
-SELECT DISTINCT student.id AS student , COUNT(sibling.student_id) AS siblings
-FROM student, sibling
-WHERE student.person_id = sibling.person_id
+SELECT COUNT(student) AS students,
+CASE 
+WHEN siblings = 1  THEN 'Has 1 sibling'
+WHEN siblings > 1  THEN 'Has atleast 2 siblings'
+ELSE 'Has no siblings'
+END AS amount_of_siblings 
+FROM(
+SELECT DISTINCT student.id AS student,
+COUNT(sibling.student_id) AS siblings
+FROM student
+FULL OUTER JOIN sibling 
+ON student.person_id = sibling.person_id
 GROUP BY student
-ORDER BY student;
+ORDER BY student
+) AS f
+GROUP BY siblings;
+
 
 /* 
- student | siblings 
----------+----------
-   21111 |        1
-   21112 |        1
-   21119 |        2
-   21120 |        2
-   21121 |        2 
+
+ students |   amount_of_siblings
+----------+------------------------
+       18 | Has no siblings
+        3 | Has atleast 2 siblings
+        2 | Has 1 sibling
+
 */
 
 
@@ -134,7 +132,7 @@ AS f
 WHERE CAST(month AS INT) = (date_part('month',CURRENT_DATE))
 ORDER BY instructor
 ) as g
-WHERE (amount_of_lessons > 1);
+WHERE (amount_of_lessons > 0);
 
 
 /*Month can be changed to an int for testing */
@@ -163,9 +161,9 @@ instructor | amount_of_lessons | month
 
 SELECT id AS lesson_id, 
 CASE 
-WHEN places_left > 2 THEN 'Many seats left'
-WHEN places_left > 0 THEN '1-2 seats left'
-ELSE 'No seats left'
+WHEN places_left > 2 THEN 'Many spots left'
+WHEN places_left > 0 THEN '1-2 spots left'
+ELSE 'No spots left'
 END AS places_left, genre, schedule, day , month
 FROM (
 SELECT DISTINCT id, 
@@ -175,7 +173,8 @@ COUNT(student_attending) OVER (PARTITION BY id) AS students_attending,
 day, month  
 FROM 
 (
-SELECT ensemble.id, student_id AS student_attending, CAST(ensemble.max_students AS INT) AS max, 
+SELECT ensemble.id, student_id AS student_attending, 
+CAST(ensemble.max_students AS INT) AS max, 
 ensemble.day, ensemble.month, genre, schedule
 FROM ensemble 
 FULL OUTER JOIN  scheduled_ensemble
@@ -192,13 +191,14 @@ ORDER BY genre_id, day
 
 
 /*
+
 Final result 
  lesson_id |   places_left   |  genre  | schedule | day | month
 -----------+-----------------+---------+----------+-----+-------
-     22006 | 1-2 seats left  | RnB     | Friday   | 02  | 12
-     22007 | Many seats left | RnB     | Monday   | 05  | 12
-     22008 | Many seats left | Grunge  | Tuesday  | 06  | 12
-     22009 | Many seats left | Hip-Hop | Tuesday  | 06  | 12
+     22006 | 1-2 spots left  | RnB     | Friday   | 02  | 12
+     22007 | Many spots left | RnB     | Monday   | 05  | 12
+     22008 | Many spots left | Grunge  | Tuesday  | 06  | 12
+     22009 | Many spots left | Hip-Hop | Tuesday  | 06  | 12
 
 Middle subquery
 
